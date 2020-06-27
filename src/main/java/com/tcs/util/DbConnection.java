43167df7cbaf3232df64a651446b6e81a29a95a9
Dell <Dell@DESKTOP>
2020-06-27 15:13:02 +0530

    Database Connection methods are added

--- src/main/java/com/tcs/util/DbConnection.java
+++ src/main/java/com/tcs/util/DbConnection.java
@@ -1,5 +1,57 @@
 package com.tcs.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
 public class DbConnection {
 
-}
+		private static final String URL = "jdbc:mysql://localhost:3306/hms";
+		private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+		private static final String USERNAME = "root";
+		private static final String PASSWORD = "root";
+
+		public static Connection getConnection() {
+			Connection con = null;
+			
+			try {
+				Class.forName(DRIVER_NAME);
+				con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+				
+			}catch(ClassNotFoundException e){
+				e.printStackTrace();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+			return con;
+		}
+
+		public static void closeConnection(Connection con) {
+			if(con!=null) {
+				try {
+					con.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+
+		public static void closeStatement(PreparedStatement ps) {
+			// TODO Auto-generated method stub
+			if(ps!=null) {
+				try {
+					ps.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
