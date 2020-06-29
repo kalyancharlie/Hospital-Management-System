5bff9fdb1f9c303b497ccfd7ac0c44a3be435825
kalyancharlie <kalyancharlie@gmail.com>
2020-06-29 11:26:33 +0530

    Depricated DB Methods Removed

--- src/main/java/com/tcs/util/DbConnection.java
+++ src/main/java/com/tcs/util/DbConnection.java
@@ -1,57 +1,46 @@
 package com.tcs.util;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-
-
 public class DbConnection {
-
-		private static final String URL = "jdbc:mysql://localhost:3306/hms";
-		private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-		private static final String USERNAME = "root";
-		private static final String PASSWORD = "!9492234713@";
-
-		public static Connection getConnection() {
-			Connection con = null;
+	private static final String URL = "jdbc:mysql://localhost:3306/hms";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
+	
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	public static void closeConnection(Connection con) {
+		if(con!=null) {
 			try {
-				Class.forName(DRIVER_NAME);
-				con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-				
-			}catch(ClassNotFoundException e){
-				e.printStackTrace();
+				con.close();
 			}catch(SQLException e) {
 				e.printStackTrace();
 			}
-			
-			return con;
 		}
-
-		public static void closeConnection(Connection con) {
-			if(con!=null) {
-				try {
-					con.close();
-				}catch(SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			
-		}
-
-		public static void closeStatement(PreparedStatement ps) {
-			// TODO Auto-generated method stub
-			if(ps!=null) {
-				try {
-					ps.close();
-				}catch(SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
+		
 	}
+	
+	public static void closeStatement(PreparedStatement ps) {
+		if(ps!=null) {
+			try {
+				ps.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
 
