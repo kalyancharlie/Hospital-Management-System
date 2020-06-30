9a62af017d372247a2baf25d7b762286c55edc0a
Kumar Raju <kottakotakumarraju999@gmail.com>
2020-06-30 16:44:31 +0530

    added date difference method

--- src/main/java/com/tcs/util/Utility.java
+++ src/main/java/com/tcs/util/Utility.java
@@ -1,9 +1,14 @@
 package com.tcs.util;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Date;
 
 public class Utility {
 	public static int numberOfDays(Date date) {
-		return 0;
+		LocalDate prev = LocalDate.of(date.getYear(), date.getMonth(), date.getDay());
+		 LocalDate now = LocalDate.now();
+		int numDays = Period.between(prev, now).getDays();
+		return numDays;
 	}
 }
